3fe49b8b54e2be7396cf2b3541945d61b9fc11c7
cia2003 <graciansamosir@gmail.com>
2026-01-02 17:08:59 +0800

    add application definition

--- dico_event/settings.py
+++ dico_event/settings.py
@@ -37,6 +37,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'events.apps.EventsConfig',
+    'tickets.apps.TicketsConfig',
+    'registrations.apps.RegistrationsConfig',
+    'payments.apps.PaymentsConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
