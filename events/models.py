8f2292e838137073659994b107f3857e96f8294e
cia2003 <graciansamosir@gmail.com>
2026-01-02 16:03:02 +0800

    add dit ignore

--- events/models.py
+++ events/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Event(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    datetime = models.DateTimeField()
+    location = models.CharField(max_length=300)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
