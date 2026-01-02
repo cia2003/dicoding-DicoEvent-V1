8938de0867b92f8f46fc64a4eb94aec6441fccfe
cia2003 <graciansamosir@gmail.com>
2026-01-02 15:31:30 +0800

    migration

--- dico_event/settings.py
+++ dico_event/settings.py
@@ -16,7 +16,7 @@ import os
 
 
 # Load environment variables from .env.var file
-load_dotenv()
+load_dotenv('.env.var')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
