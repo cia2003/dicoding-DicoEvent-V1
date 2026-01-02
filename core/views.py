a94f9b9fb84b792fca843dace2977cbe5a4b55de
cia2003 <graciansamosir@gmail.com>
2026-01-02 17:37:06 +0800

    add views and serializers

--- core/views.py
+++ core/views.py
@@ -1,3 +1,93 @@
 # from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+from django.http import Http404
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import Group
+from .models import User
+from .serializers import UserSerializer
+from .serializers import GroupSerializer
+ 
+class UserListCreateView(APIView):
+    def get(self, request):
+        users = User.objects.all().order_by('username')[:10]
+        serializer = UserSerializer(users, many=True)
+        return Response({'users': serializer.data})
+ 
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+ 
+class UserDetailView(APIView):
+    def get_object(self, pk):
+        try:
+            user = User.objects.get(pk=pk)
+            self.check_object_permissions(self.request, user)
+            return user
+        except User.DoesNotExist:
+            raise Http404
+ 
+    def get(self, request, pk):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+ 
+    def put(self, request, pk):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+ 
+    def delete(self, request, pk):
+        user = self.get_object(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class GroupListCreateView(APIView):
+    def get(self, request):
+        groups = Group.objects.all().order_by('name')[:10]
+        serializer = GroupSerializer(groups, many=True)
+        return Response({'groups': serializer.data})
+ 
+    def post(self, request):
+        serializer = GroupSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+ 
+class GroupDetailView(APIView):
+    def get_object(self, pk):
+        try:
+            group = Group.objects.get(pk=pk)
+            self.check_object_permissions(self.request, group)
+            return group
+        except Group.DoesNotExist:
+            raise Http404
+ 
+    def get(self, request, pk):
+        group = self.get_object(pk)
+        serializer = GroupSerializer(group)
+        return Response(serializer.data)
+ 
+    def put(self, request, pk):
+        group = self.get_object(pk)
+        serializer = GroupSerializer(group, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+ 
+    def delete(self, request, pk):
+        group = self.get_object(pk)
+        group.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
