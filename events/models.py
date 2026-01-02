894738cfc6a1a9375146bdb99ee0c52e59ca2d88
cia2003 <graciansamosir@gmail.com>
2026-01-02 16:58:38 +0800

    add Event Model

--- events/models.py
+++ events/models.py
@@ -5,9 +5,14 @@ import uuid
 class Event(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200)
-    datetime = models.DateTimeField()
-    location = models.CharField(max_length=300)
     description = models.TextField()
+    location = models.CharField(max_length=300)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    status = models.CharField(max_length=50)
+    quota = models.IntegerField()
+    category = models.CharField(max_length=100)
+    organizer_id = models.UUIDField()
 
     def __str__(self):
         return self.name
