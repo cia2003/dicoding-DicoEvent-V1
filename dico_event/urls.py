ea64bc430cc415ebe3b3b21c969b67ab69d6bce6
cia2003 <graciansamosir@gmail.com>
2026-01-02 16:56:47 +0800

    update environment variables

--- dico_event/urls.py
+++ dico_event/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('events.urls')),
+    # path('api/', include('events.urls')),
     # path
 ]
--- events/models.py
+++ events/models.py
@@ -11,3 +11,6 @@ class Event(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        db_table = 'events'
